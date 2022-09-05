12f708ea63aa6ebe9c4e025c3aa2cb8c77968b37
Carlos Serôdio <carlosserodio86@hotmail.com>
2022-09-05 16:19:33 +0100

    Implement Payment by PayPal

---
+++ pages/api/orders/[id]/pay.js
@@ -0,0 +1,37 @@
+import { getSession } from 'next-auth/react';
+import Order from '../../../../models/Order';
+import db from '../../../../utils/db';
+
+// create handler function
+const handler = async (req, res) => {
+  // get session from getSession method
+  const session = await getSession({ req });
+
+  // check session
+  if (!session) {
+    return res.status(401).send('Error: signin required');
+  }
+
+  await db.connect();
+  const order = await Order.findById(req.query.id);
+  if (order) {
+    if (order.isPaid) {
+      return res.status(400).send({ message: 'Error: order is already paid' });
+    }
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      email_address: req.body.email_address,
+    };
+    const paidOrder = await order.save();
+    await db.disconnect();
+    res.send({ message: 'order paid successfully', order: paidOrder });
+  } else {
+    await db.disconnect();
+    res.status(404).send({ message: 'Error: order not found' });
+  }
+};
+
+export default handler;
