13cfec194906f2521ecc41d71400b6fb9dc96475
Carlos Serôdio <carlosserodio86@hotmail.com>
2022-09-21 11:39:03 +0100

    Comments

--- pages/api/products/categories.js
+++ pages/api/products/categories.js
@@ -2,18 +2,18 @@ import Product from '../../../models/Product';
 import db from '../../../utils/db';
 
 // define handler ( async function )
-// create an api to get the product from backend
+// create an api to get the product categories from backend
 const handler = async (
   req, // 1st parameter - request
   res // 2nd parameter - response
 ) => {
   // connect to the database
   await db.connect();
-  /* get the product in the database using find method */
+  /* get the categories in the database using find method */
   const categories = await Product.find().distinct('category');
   // disconnect from the database
   await db.disconnect();
-  //return the product to the frontend
+  //return the categories to the frontend
   res.send(categories);
 };
 
